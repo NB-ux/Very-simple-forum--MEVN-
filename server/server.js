bc857406349ec9963d96e138bc70971f1cfb868a
Niko <niko.bagaew@gmail.com>
2022-09-06 23:54:08 +0300

    base for the application is ready: mevn generate - crud boilerplate

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 // Importing required modules
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
+
+//MongoDb Connection string
+
+require('dotenv').config({ path: '.env' });
+
+require("./helpers/db/mongodb.js")();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  process.env.DB_URL;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 // parse env variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Configuring port
 const port = process.env.PORT || 9000;
@@ -14,13 +34,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.set('view engine', 'html');
+app.set("view engine", "html");
 
 // Static folder
-app.use(express.static(__dirname + '/views/'));
+app.use(express.static(__dirname + "/views/"));
 
 // Defining route middleware
-app.use('/api', require('./routes/api'));
+app.use("/api", require("./routes/api"));
 
 // Listening to port
 app.listen(port);
