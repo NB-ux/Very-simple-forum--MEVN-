ef418c9402b36f1239a9ce42198bfa1e308581c8
Niko Bagaev <niko.bagaew@gmail.com>
2023-04-04 03:00:04 +0300

    client and server are now in root dir

---
+++ client/src/components/Piece.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+    <a>Here is a piece of paper!!!</a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Piece"
+};
+</script>
---
+++ client/src/views/Paper.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="paper">
+      <img alt="Vue logo" src="../assets/mevn.jpg" width="350" />
+      <Piece />
+      <a>test</a>
+    </div>
+</template>
+  
+  <script>
+  // @ is an alias to /src
+  import Piece from "@/components/Piece.vue";
+  
+  export default {
+    name: "paper",
+    components: {
+      Piece
+    }
+  };
+  </script>
+  
