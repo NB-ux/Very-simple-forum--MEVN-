3e4d58e26792e0178146e91b2b85d678a2364507
Niko Bagaev <niko.bagaew@gmail.com>
2025-04-20 03:07:53 +0300

    migrated to vue 3.5

--- client/src/router.js
+++ client/src/router.js
@@ -1,27 +1,27 @@
-import Vue from "vue";
-import Router from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 
-Vue.use(Router);
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("./views/About.vue"),
+  },
+  {
+    path: "/paper",
+    name: "paper",
+    component: () => import("./views/Paper.vue"),
+  },
+];
 
-export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("./views/About.vue")
-    },
-    {
-      path: "/paper",
-      name: "paper",
-      component: () => import("./views/Paper.vue")
-    }
-  ]
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
+
+export default router;
