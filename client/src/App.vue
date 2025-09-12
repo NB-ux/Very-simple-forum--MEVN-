9c792ec6945aa9dcf9bfd7432dc20bdf017083ad
Niko Bagaev <niko.bagaew@gmail.com>
2025-09-12 16:31:26 +0300

    empty login page added

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/paper">Roll</router-link>
+      <router-link to="/paper">Roll</router-link> |
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view />
   </div>
@@ -29,5 +30,4 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-
 </style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login-container">
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="username" type="text" placeholder="Username" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Log In</button>
+    </form>
+    <p v-if="error" class="error">{{ error }}</p>
+  </div>
+</template>
+
+<script></script>
