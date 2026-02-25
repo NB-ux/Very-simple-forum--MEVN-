434db856638c61a6428af0b8d9e45b0a2bc20f91
NB-ux <niko.bagaew@gmail.com>
2026-02-25 13:54:47 +0200

    Disabled useless tabs

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "./views/Home.vue";
+//import Home from "./views/Home.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: Home,
+    component: () => import("./views/Paper.vue"),
   },
   {
     path: "/about",
